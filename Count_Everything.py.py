1a9faa2ad9e8fc92739ce4da736532ac3f83523f
Veda-Verse <97297180+Veda-Verse@users.noreply.github.com>
2022-01-07 20:09:52 +0530

    Add files via upload
    This is very good project for bignners .
    In this project You have to type/paste your text and project will count the number of each alphabet used in the text , it will also count underscores , Space , Special characters etc.
    You must try.

---
+++ Count_Everything.py.py
@@ -0,0 +1,109 @@
+a=input(str('Paste Your text: '))
+acount =a.count("a" and "A")
+print('No of a is: ', (acount))
+
+bcount =a.count("b" and "B")
+print('No of b is: ', (bcount))
+
+ccount =a.count("c" and "C")
+print('No of c is: ', (ccount))
+
+dcount =a.count("d"and"D")
+print('No of d is: ', (dcount))
+
+ecount =a.count("e" and "E")
+print('No of e is: ', (ecount))
+
+fcount =a.count("f" and "F")
+print('No of f is: ', (fcount))
+
+gcount =a.count("g" and "G")
+print('No of g is: ', (gcount))
+
+hcount =a.count("h" and "H")
+print('No of h is: ', (hcount))
+
+icount =a.count("i" and "I")
+print('No of i is: ', (icount))
+
+jcount =a.count("j" and "J")
+print('No of j is: ', (jcount))
+
+kcount =a.count("k" and "K")
+print('No of k is: ', (kcount))
+
+lcount =a.count("l" and "L")
+print('No of l is: ', (lcount))
+
+mcount =a.count("m" and "M")
+print('No of m is: ', (mcount))
+
+
+ncount =a.count("n" and "N")
+print('No of n is: ', (ncount))
+
+ocount =a.count("o" and "O")
+print('No of o is: ', (ocount))
+
+pcount =a.count("p" and "P")
+print('No of p is: ', (pcount))
+
+qcount =a.count("q" and"Q")
+print('No of q is: ', (qcount))
+
+rcount =a.count("r" and "R")
+print('No of r is: ', (rcount))
+
+scount =a.count("s" and "S")
+print('No of s is: ', (scount))
+
+tcount =a.count("t" and "T")
+print('No of t is: ', (tcount))
+
+ucount =a.count("u" and "U")
+print('No of u is: ', (ucount))
+
+vcount =a.count("v" and "V")
+print('No of v is: ', (vcount))
+
+wcount =a.count("w" and "W")
+print('No of w is: ', (wcount))
+
+xcount =a.count("x" and "V")
+print('No of x is: ', (xcount))
+
+ycount =a.count("y" and "Y")
+print('No of y is: ', (ycount))
+
+zcount =a.count("z" and "Z")
+print('No of z is: ', (zcount))
+
+fullstopcount=a.count(".")
+print('No. of fullstops(.) are: ',(fullstopcount))
+
+spacecount= a.count(' ')
+print('No. of space: ', spacecount)
+
+_count=a.count("_")
+print('No. of _ is', _count)
+
+semicoloncount=a.count(";")
+print('No. of semi colon(;) is ', semicoloncount)
+
+coloncount=a.count(":")
+print('No. of colon(:) is ', coloncount)
+
+extracharacters=a.count("!" and "#" and "@" and "?" and ">" and "<" and "}" and "{" and ")" and "(" and "%" and "£" and "*" and "&" and "^" and "$" and "+" and "=" and "-" and "/" and "`" and "¬")
+print('No. of Extra characyers are :',extracharacters)
+
+print('''Thanks For using us!!!!!!!!!!
+
+
+         o                 o        *********   *        *          *         *       *    *   *     * * * *
+                _                       *       *        *         *  *       * *     *    * *       *
+         .                .             *       * * * *  *        *    *      *   *   *    **        * * * *
+          .              .              *       *        *       * * *  *     *     * *    * *             *
+            .          .                *       *        *      *        *    *       *    *   *     * * * *   
+              .      .                     
+                  .     ''')
+                
